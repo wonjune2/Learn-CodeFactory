c8fbdd949673fd860c7618a7511be9594a073642
Joon <zzzdnjswns@naver.com>
2023-02-25 23:07:10 +0900

    hello codefactory

---
+++ ch04/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: Text(
+          'Hello Code Factory',
+        ),
+      ),
+    ),
+  );
+}
