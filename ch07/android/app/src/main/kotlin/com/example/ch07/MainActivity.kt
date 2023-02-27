51d46344ee23a0b44a8a0bcbdbd3e15a4a7a2fa6
Joon <zzzdnjswns@naver.com>
2023-02-27 21:59:44 +0900

    ch07

---
+++ ch07/android/app/src/main/kotlin/com/example/ch07/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ch07
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ch07/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:ch07/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
