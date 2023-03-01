96b3a701056e52861a41e8ef2df5658da5e943c0
Joon <zzzdnjswns@naver.com>
2023-03-02 01:11:30 +0900

    CupertinoDate Picker

--- ch09/lib/screen/home_screen.dart
+++ ch09/lib/screen/home_screen.dart
@@ -1,7 +1,66 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  DateTime firstDay = DateTime.now();
+
+  void onHeartPressed() {
+    showCupertinoDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            color: Colors.white,
+            height: 300,
+            child: CupertinoDatePicker(
+              mode: CupertinoDatePickerMode.date,
+              onDateTimeChanged: (DateTime date) {
+                setState(() {
+                  firstDay = date;
+                });
+              },
+            ),
+          ),
+        );
+      },
+      barrierDismissible: true,
+    );
+  }
+
+  // void onHeartPressed() {
+  //   // ➍ 하트 눌렀을때 실행할 함수
+  //   showCupertinoDialog(
+  //     // ➋ 쿠퍼티노 다이얼로그 실행
+  //     context: context,
+  //     builder: (BuildContext context) {
+  //       return Align(
+  //         // ➊ 정렬을 지정하는 위젯
+  //         alignment: Alignment.bottomCenter, // ➋ 아래 중간으로 정렬
+  //         child: Container(
+  //           color: Colors.white, // 배경색 흰색 지정
+  //           height: 300, // 높이 300 지정
+  //           child: CupertinoDatePicker(
+  //             mode: CupertinoDatePickerMode.date,
+  //             onDateTimeChanged: (DateTime date) {
+  //               setState(() {
+  //                 firstDay = date;
+  //               });
+  //             },
+  //           ),
+  //         ),
+  //       );
+  //     },
+  //     barrierDismissible: true,
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +71,12 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            _DDay(),
-            _CoupleImage(),
+          children: [
+            _DDay(
+              onHeartPressed: onHeartPressed,
+              firstDay: firstDay,
+            ),
+            const _CoupleImage(),
           ],
         ),
       ),
@@ -23,37 +85,52 @@ class HomeScreen extends StatelessWidget {
 }
 
 class _DDay extends StatelessWidget {
-  const _DDay();
+  final GestureTapCallback onHeartPressed;
+  final DateTime firstDay;
+
+  const _DDay({
+    required this.onHeartPressed, // ➋ 상위에서 함수 입력받기
+    required this.firstDay,
+  });
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
+    final now = DateTime.now();
+
     return Column(
       children: [
         const SizedBox(height: 16.0),
         Text(
+          // 최상단 U&I 글자
           'U&I',
           style: textTheme.displayLarge,
         ),
         const SizedBox(height: 16.0),
         Text(
+          // 두번째 글자
           '우리 처음 만난 날',
           style: textTheme.bodyLarge,
         ),
         Text(
-          '2021.11.23',
+          // 임시로 지정한 만난 날짜
+          '${firstDay.year}.${firstDay.month}.${firstDay.day}',
           style: textTheme.bodyMedium,
         ),
         const SizedBox(height: 16.0),
         IconButton(
+          // 하트 아이콘 버튼
           iconSize: 60.0,
-          onPressed: () {},
-          icon: const Icon(Icons.favorite),
-          color: Colors.red,
+          onPressed: onHeartPressed,
+          icon: const Icon(
+            Icons.favorite,
+            color: Colors.red,
+          ),
         ),
         const SizedBox(height: 16.0),
         Text(
-          'D+365',
+          // 만난 후 DDay
+          'D+${DateTime(now.year, now.month, now.day).difference(firstDay).inDays + 1}',
           style: textTheme.displayMedium,
         ),
       ],
@@ -66,10 +143,12 @@ class _CoupleImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image.asset(
-        'assets/img/middle_image.png',
-        height: MediaQuery.of(context).size.height / 2,
+    return Expanded(
+      child: Center(
+        child: Image.asset(
+          'assets/img/middle_image.png',
+          height: MediaQuery.of(context).size.height / 2,
+        ),
       ),
     );
   }
