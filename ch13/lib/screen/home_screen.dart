141ba00fd2b02bf041f427657d4b43d3aa962be9
Joon <zzzdnjswns@naver.com>
2023-03-06 23:07:16 +0900

    googleMaps

--- ch13/lib/screen/home_screen.dart
+++ ch13/lib/screen/home_screen.dart
@@ -1,11 +1,64 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  static const LatLng companyLatLng = LatLng(
+    37.5233273,
+    126.921252,
+  );
+
   const HomeScreen({super.key});
+
+  AppBar renderAppBar() {
+    return AppBar(
+      centerTitle: true,
+      title: const Text(
+        '오늘도 출첵',
+        style: TextStyle(
+          color: Colors.blue,
+          fontWeight: FontWeight.w700,
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: renderAppBar(),
+      body: Column(
+        children: [
+          const Expanded(
+            flex: 2,
+            child: GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: companyLatLng,
+                zoom: 16,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(
+                  Icons.timelapse_outlined,
+                  color: Colors.blue,
+                  size: 50.0,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('출근하기'),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
