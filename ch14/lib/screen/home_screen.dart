4c1cddcb6722b5ed5474b9dcc6f32451d249d67f
Joon <zzzdnjswns@naver.com>
2023-03-10 01:41:55 +0900

    renderBody

--- ch14/lib/screen/home_screen.dart
+++ ch14/lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:ch14/component/main_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -15,8 +17,9 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        fit: StackFit.expand, // 자식 위젯들 최대 크기로 펼치기
+        fit: StackFit.expand,
         children: [
+          renderBody(),
           Positioned(
             top: 0,
             left: 0,
@@ -30,6 +33,33 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
     );
+  }
+
+  Widget renderBody() {
+    if (image != null) {
+      // Stack 크기의 최대 크기만큼 차지하기
+      return Positioned.fill(
+        // 위젯 확대 및 좌우 이동을 가능하게 하는 위젯
+        child: InteractiveViewer(
+          child: Image.file(
+            File(image!.path),
+
+            // 이미지가 부모 위젯 크기 최대를 차지하도록 하기
+            fit: BoxFit.cover,
+          ),
+        ),
+      );
+    } else {
+      return Center(
+        child: TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.grey,
+          ),
+          onPressed: onPickImage,
+          child: const Text('이미지 선택하기'),
+        ),
+      );
+    }
   }
 
   void onPickImage() async {
