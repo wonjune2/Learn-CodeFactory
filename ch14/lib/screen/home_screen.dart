70d27bdb29903b87eb77a6ff3611e195e61908d6
Joon <zzzdnjswns@naver.com>
2023-03-11 22:20:46 +0900

    ch14

--- ch14/lib/screen/home_screen.dart
+++ ch14/lib/screen/home_screen.dart
@@ -1,19 +1,29 @@
 import 'dart:io';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 
+import 'package:ch14/component/emoticon_sticker.dart';
 import 'package:ch14/component/footer.dart';
 import 'package:ch14/component/main_app_bar.dart';
+import 'package:ch14/model/sticker_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:uuid/uuid.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
   XFile? image;
+  Set<StickerModel> stickers = {};
+  String? selectedId;
+  GlobalKey imgKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,14 +56,36 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget renderBody() {
     if (image != null) {
       // Stack 크기의 최대 크기만큼 차지하기
-      return Positioned.fill(
-        // 위젯 확대 및 좌우 이동을 가능하게 하는 위젯
-        child: InteractiveViewer(
-          child: Image.file(
-            File(image!.path),
-
-            // 이미지가 부모 위젯 크기 최대를 차지하도록 하기
-            fit: BoxFit.cover,
+      return RepaintBoundary(
+        key: imgKey,
+        child: Positioned.fill(
+          // 위젯 확대 및 좌우 이동을 가능하게 하는 위젯
+          child: InteractiveViewer(
+            child: Stack(
+              fit: StackFit.expand,
+              children: [
+                Image.file(
+                  File(image!.path),
+                  // 이미지가 부모 위젯 크기 최대를 차지하도록 하기
+                  fit: BoxFit.cover,
+                ),
+                ...stickers.map(
+                  (sticker) => Center(
+                    child: EmoticonSticker(
+                      key: ObjectKey(
+                        sticker.id,
+                      ),
+                      onTransform: () {
+                        onTransform(sticker.id);
+                        // 스티커의 ID값 함수의 매개변수로 전달
+                      },
+                      imgPath: sticker.imgPath,
+                      isSelected: selectedId == sticker.id,
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       );
@@ -70,7 +102,23 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  void onEmotioconTap(int index) {}
+  void onTransform(String id) {
+    setState(() {
+      selectedId = id;
+    });
+  }
+
+  void onEmotioconTap(int index) async {
+    setState(() {
+      stickers = {
+        ...stickers,
+        StickerModel(
+          id: const Uuid().v4(),
+          imgPath: 'asset/img/emoticon_$index.png',
+        ),
+      };
+    });
+  }
 
   void onPickImage() async {
     final image = await ImagePicker().pickImage(
@@ -82,7 +130,25 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
-  void onSaveImage() {}
+  void onSaveImage() async {
+    RenderRepaintBoundary boundary =
+        imgKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+    ui.Image image = await boundary.toImage();
+    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+    Uint8List pngBytes = byteData!.buffer.asUint8List();
 
-  void onDeleteItem() {}
+    await ImageGallerySaver.saveImage(pngBytes, quality: 100);
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('저장 되었습니다.'),
+      ),
+    );
+  }
+
+  void onDeleteItem() async {
+    setState(() {
+      stickers = stickers.where((sticker) => sticker.id != selectedId).toSet();
+    });
+  }
 }
